64c50ac0a47e7ac0ce6eb1ba6aa8742a11c338ab
Joel Turk <jturk@udel.edu>
2018-06-18 20:46:52 -0400

    Initial Commit

---
+++ Shride/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shride
+//
+//  Created by Joel Turk on 6/18/18.
+//  Copyright © 2018 Shride. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
